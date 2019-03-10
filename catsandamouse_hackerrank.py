408016daddd3867b4e3533548cec0d3928c4b2fd
Kartik Tanksali <kartik.tanksali@gmail.com>
2019-03-10 16:32:41 -0700

    Cat and Mouse Problem - Implementation task

---
+++ catsandamouse_hackerrank.py
@@ -0,0 +1,36 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the catAndMouse function below.
+def catAndMouse(x, y, z):
+    dis_a = abs(x-z)
+    dis_b = abs(y-z)
+    if dis_a==dis_b:
+        return "Mouse C"
+    elif dis_a<dis_b:
+        return "Cat A"
+    else:
+        return "Cat B"
+    
+
+if __name__ == '__main__':
+
+    q = int(input())
+
+    for q_itr in range(q):
+        xyz = input().split()
+
+        x = int(xyz[0])
+
+        y = int(xyz[1])
+
+        z = int(xyz[2])
+
+        result = catAndMouse(x, y, z)
+
+        print(result)
